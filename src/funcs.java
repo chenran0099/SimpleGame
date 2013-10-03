a82d41350f1748aa3e588f15287f57c2a95f2cf2
MaomiHz <MaomiHz@MaomiHz.local>
2013-10-03 14:01:31 +0800

    update version 1.1.1

--- src/funcs.java
+++ src/funcs.java
@@ -1,7 +1,7 @@
 
 public class funcs {
 
-	
+
 	public static int getRandom() {
 		int result = 0;
 		for(int i = 0; i<3;i++){
@@ -11,8 +11,16 @@ public class funcs {
 	}
 	
 	public static void outStatus(int money, int play){
-		System.out.printf("Money: $%s.\n", money);
-		System.out.printf("Total play: %s\n", play);
+		System.out.printf("Present Money: $%d.\n", money);
+		System.out.printf("Total play: %d\n", play);
+	}
+	// true for plus and false for minus;
+	public static void outStatus(int money, int play, boolean b, int moneyChange){
+		if (b)
+			System.out.printf("Present Money: $%d(+$%d).\n", money, moneyChange);
+		else 
+			System.out.printf("Present Money: $%d\n", money);
+			System.out.printf("Total play: %d\n", play);
 	}
 	
 	
@@ -22,6 +30,8 @@ public class funcs {
 		if (choice == 2)
 			System.out.println("* Your choice is 11~18.");
 		if (choice >= 3 && choice <= 18)
-			System.out.printf("* Your choice is %s. \n", choice);
+			System.out.printf("* Your choice is %d. \n", choice);
 	}
+	
+	
 }
--- src/startgame.java
+++ src/startgame.java
@@ -11,7 +11,7 @@ public class startgame {
 		int randomNum;
 		int moneyChange;
 		int win = 0;
-		
+		boolean status = true;
 		int arr[] = {0,2,2,216,72,36,21,14,10,8,8,8,8,10,14,21,36,72,216};
 		//object
 		Scanner bucky = new Scanner(System.in);
@@ -20,44 +20,53 @@ public class startgame {
 		//main
 		funcObj.outStatus(money, play);
 		System.out.println("Game instruction: Please check https://github.com/maomihz.\nLET'S PLAY.\n*********************\n");
-		while(money > 50){
-			money -= 50;
-			System.out.println("Money-50\nMoney: $" + money);
+		while(money > 0){
+			System.out.printf("Money: $%d\n", money);
 			do {
-				System.out.print("Which number you bet>> ");
+				System.out.print("Which number you bet>>");
 				userIn = bucky.nextInt();
 				System.out.print("How much you bet>>");
 				moneyChange = bucky.nextInt();
-				if (userIn <= 0 || userIn > 18 || moneyChange > money)
+
+				if (userIn <= 0 || userIn > 18 || moneyChange > money){
 					System.out.println("input wrong, please retry.");
+					continue;
+				}
 				if (userIn == 0) {
 					System.out.println("Cheat enabled, money+++");
 					money += moneyChange;
-					System.out.println("Money = " + money);
+					System.out.printf("Money = %d", money);
+				} else {
+					money -= moneyChange/10 + 1;
+					money -= moneyChange;
+					System.out.printf("Money: $%d(-$%d).\n", money, moneyChange + moneyChange / 10 + 1);
+					break;
 				}
 					
-			} while (userIn <= 0 || userIn > 18 || moneyChange > money);
+			} while (true);
 			
-			money -= moneyChange;
+			
 			randomNum = funcObj.getRandom();
 			if ((userIn == 1 && randomNum <= 10) || (userIn == 2 && randomNum > 10) || (userIn >= 3 && userIn <= 18 && userIn == randomNum)){
-				money += moneyChange * arr[userIn];
-				win += moneyChange * arr[userIn];
+				moneyChange *= arr[userIn];
+				money += moneyChange;
+				win += moneyChange;
 				funcObj.outChoice(userIn);
-				System.out.println("* You win, the number is " + randomNum + "\nMoney+$" + moneyChange * arr[userIn]);
+				status = true;
+				System.out.printf("* You win, the number is %d\n", randomNum);
 			} else {
 				funcObj.outChoice(userIn);
-				System.out.println("* You lose, the number is " + randomNum);
+				status = false;
+				System.out.printf("* You lose, the number is %d\n", randomNum);
 			}
 
 			
 			
 			play++;
-			funcObj.outStatus(money, play);
-			System.out.println("\n\n");
-			System.out.println("*********************");
+			funcObj.outStatus(money, play, status, moneyChange);
+			System.out.print("\n\n\n*********************\n");
 		}
-		System.out.println("GAME OVER\nYour score is: " + win);
+		System.out.printf("GAME OVER\nYour score is: %d\n", win);
 		
 	}
 
